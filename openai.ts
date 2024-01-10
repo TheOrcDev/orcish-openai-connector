6d97c38eaac6a6e0b073cfe535aff55ebd26b663
papaurke <urosmiric88@gmail.com>
2024-01-10 21:10:06 +0100

    Proper types

--- openai.ts
+++ openai.ts
@@ -15,55 +15,44 @@ export class OpenAIService {
     };
   }
 
-  async getChatGPTCompletion(input) {
-    try {
-      const response = await fetch(
-        "https://api.openai.com/v1/chat/completions",
-        {
-          method: "POST",
-          headers: this.headers,
-          body: JSON.stringify({
-            model: process.env.OPENAI_GPT_MODEL ?? "gpt-3.5-turbo",
-            messages: [{ role: "user", content: input }],
-            temperature: parseInt(process.env.OPENAI_GPT_TEMPERATURE ?? "0.8"),
-            max_tokens: parseInt(process.env.OPENAI_GPT_MAX_TOKENS ?? "1048"),
-          }),
-        }
-      );
+  async getChatGPTCompletion(input: string): Promise<string> {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({
+        model: process.env.OPENAI_GPT_MODEL ?? "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input }],
+        temperature: parseInt(process.env.OPENAI_GPT_TEMPERATURE ?? "0.8"),
+        max_tokens: parseInt(process.env.OPENAI_GPT_MAX_TOKENS ?? "1048"),
+      }),
+    });
 
-      const data = await response.json();
+    const data = await response.json();
 
-      if (data.choices[0].finish_reason === "length") {
-        throw new Error("Too long prompt");
-      }
-
-      return data.choices[0].message.content;
-    } catch (error) {
-      throw error;
+    if (data.choices[0].finish_reason === "length") {
+      throw new Error("Too long prompt");
     }
+
+    return data.choices[0].message.content;
   }
 
-  async getDalle3Image(prompt) {
-    try {
-      const response = await fetch(
-        "https://api.openai.com/v1/images/generations",
-        {
-          method: "POST",
-          headers: this.headers,
-          body: JSON.stringify({
-            model: process.env.OPENAI_IMAGE_MODEL,
-            prompt,
-            n: 1,
-            size: process.env.OPENAI_IMAGE_RESOLUTION ?? "1792x1024",
-          }),
-        }
-      );
+  async getDalle3Image(prompt: string): Promise<string> {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: this.headers,
+        body: JSON.stringify({
+          model: process.env.OPENAI_IMAGE_MODEL,
+          prompt,
+          n: 1,
+          size: process.env.OPENAI_IMAGE_RESOLUTION ?? "1792x1024",
+        }),
+      }
+    );
 
-      const data = await response.json();
+    const data = await response.json();
 
-      return data.data[0].url;
-    } catch (error) {
-      throw error;
-    }
+    return data.data[0].url;
   }
 }
